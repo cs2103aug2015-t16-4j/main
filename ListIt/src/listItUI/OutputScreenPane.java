2df179e25587c04415d47ee74d9c31ce396462f1
zhangcs0088 <zhangcs00992008@gmail.com>
2015-10-18 17:38:13 +0800

    Implement displayDefault

--- ListIt/src/listItUI/OutputScreenPane.java
+++ ListIt/src/listItUI/OutputScreenPane.java
@@ -1,12 +1,14 @@
 package listItUI;
 
+import java.util.ArrayList;
+
 import javafx.geometry.Insets;
-import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
+import taskGenerator.Task;
 
 public class OutputScreenPane extends GridPane {
 	
@@ -31,18 +33,50 @@ public class OutputScreenPane extends GridPane {
 		getChildren().addAll(displayHeader, taskList);
 	}
 	
-	public static void displayList(Text title, Text date) {
-		GridPane task = new GridPane();
-		Label t = new Label("Title: ");
-		Label d = new Label("date: ");
+	public static void displayList(ArrayList<Task> list) {
+		Task tempTask = new Task();
 		
-		setConstraints(t, 0, 0);
-		setConstraints(d, 0, 1);
-		setConstraints(title, 1, 0);
-		setConstraints(date, 1, 1);
+		taskList.getChildren().clear();
 		
-		task.getChildren().addAll(t, d, title, date);
-		taskList.getChildren().add(task);
+		for(int i = 0; i<list.size(); i++) {
+			tempTask = list.get(i);
+			
+			GridPane taskDetail = new GridPane();
+			taskDetail = createTaskDetail(tempTask);
+			
+			taskList.getChildren().add(taskDetail);
+		}
+	}
+
+	private static GridPane createTaskDetail(Task tempTask) {
+		GridPane taskDetail = new GridPane();
+		Text taskTitle = new Text(tempTask.getTitle());
+		Text taskDeadLine = new Text(tempTask.getDate());
+		Text taskImportance = new Text(getRankingText(tempTask.getImportance()));
+		
+		GridPane.setConstraints(taskTitle, 0, 0);
+		GridPane.setConstraints(taskDeadLine, 0, 2);
+		GridPane.setConstraints(taskImportance, 2, 0);
+		
+		taskDetail.getChildren().addAll(taskTitle, taskDeadLine, taskImportance);
+		
+		return null;
+	}
+
+	private static String getRankingText(Integer importance) {
+		String rankDetail;
+		
+		if(importance == 1) {
+			rankDetail = "Very Important";
+		}
+		else if(importance == 2) {
+			rankDetail = "Important";
+		}
+		else {
+			rankDetail = "Not so Important";
+		}
+		
+		return rankDetail;
 	}
 	
 	
