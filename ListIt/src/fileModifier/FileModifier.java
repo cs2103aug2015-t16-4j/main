435aaaad3c4f1dd17ff9045393ccd405500c8d93
urvashisikka <a0127781@u.nus.edu>
2015-10-19 15:47:50 +0800

    changes to fileModifier

--- ListIt/src/fileModifier/FileModifier.java
+++ ListIt/src/fileModifier/FileModifier.java
@@ -2,6 +2,7 @@ package fileModifier;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -12,18 +13,18 @@ import taskGenerator.Task;
 public class FileModifier {
 	private static FileModifier modifier;
 	private File dataFile;
-	
+
 	private FileModifier() {
 		dataFile = new File("test1.txt");
 	}
-	
+
 	public static FileModifier getInstance() {
 		if(modifier == null) {
 			modifier = new FileModifier();
 		}
 		return modifier;
 	}
-	
+
 	public void saveFile(ArrayList<Task> dataStorage){
 		try{
 			FileOutputStream fos = new FileOutputStream("test1.txt");
@@ -34,10 +35,10 @@ public class FileModifier {
 			e.printStackTrace(); 
 		}
 	}
-	
+
 	public ArrayList<Task> getContentList() {
 		ArrayList<Task> list = new ArrayList<Task>();
-		
+
 		try{
 			FileInputStream fis = new FileInputStream("test1.txt");
 			ObjectInputStream ois = new ObjectInputStream(fis);
@@ -46,26 +47,33 @@ public class FileModifier {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return list;
 	}
 
 	public void addToFile(Task newTask){
 		ArrayList<Task> list = new ArrayList<Task>();
-		
+
 		try{
 			FileOutputStream fos = new FileOutputStream("test1.txt");
 			ObjectOutputStream oos = new ObjectOutputStream(fos);
 			FileInputStream fis = new FileInputStream("test1.txt");
 			ObjectInputStream ois = new ObjectInputStream(fis);
-			
+
 			list = (ArrayList<Task>) ois.readObject();
 			list.add(newTask);
 		}
 	}
 
-	public void deleteLine(int lineToBeDelete) {
-		
+	public void deleteLine(int lineToDelete) throws IOException {
+		FileInputStream fis = new FileInputStream("text1.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		ArrayList<Task> contents = new ArrayList<Task>();
+
+		contents = (ArrayList<Task>) ois.readObject();  
+		contents.remove(lineToDelete-1); 
+		// remove the old file if necessary 
+		saveFile(contents); 
 	}
 
 	public void displayDefault() {
@@ -79,23 +87,40 @@ public class FileModifier {
 	public File getFile() {
 		return dataFile;
 	}
-	
+
 	public void setfile(File file){
-		 dataFile = file;
+		dataFile = file;
 	}
-	
-	public void searchKeyword(String keyword) throws IOException, ClassNotFoundException{
-	ArrayList<Task> searchList = new ArrayList<Task>();
-	FileInputStream fis = new FileInputStream("test1.txt");
-	ObjectInputStream ois = new ObjectInputStream(fis);
-	
-	ArrayList<Task> contents = (ArrayList<Task>) ois.readObject();
+
+	public ArrayList<Task> searchKeyword(String keyword) throws IOException, ClassNotFoundException{
+		ArrayList<Task> searchList = new ArrayList<Task>();
+		FileInputStream fis = new FileInputStream("test1.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+
+		ArrayList<Task> contents = (ArrayList<Task>) ois.readObject();
 		for ( int i =0;i<contents.size();i++ ){
 			Task task = contents.get(i);
 			if(task.getTitle().contains(keyword)){
 				searchList.add(task);
 			}	
 		}	
-	ois.close(); 	
+		ois.close(); 	
+		return searchList; 
 	}
+
+	public ArrayList<Task> searchByImportance(int searchKey ) throws IOException, ClassNotFoundException{
+		ArrayList<Task> searchList = new ArrayList<Task>();
+		FileInputStream fis = new FileInputStream("test1.txt");
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		ArrayList<Task> contents = (ArrayList<Task>) ois.readObject();
+
+		for ( int i =0;i<contents.size();i++ ){
+			Task task = contents.get(i);
+			if(task.getImportance()==searchKey){
+				searchList.add(task);
+			}	
+		}
+		ois.close();
+		return searchList;  
+	}		
 }
