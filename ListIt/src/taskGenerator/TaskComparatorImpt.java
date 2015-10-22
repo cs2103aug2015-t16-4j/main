681730e2955121f78d8440a5c16705fe84e95e50
zhangcs0088 <zhangcs00992008@gmail.com>
2015-10-22 13:30:34 +0800

    sorting complete

--- ListIt/src/taskGenerator/TaskComparatorImpt.java
+++ ListIt/src/taskGenerator/TaskComparatorImpt.java
@@ -9,22 +9,22 @@ public class TaskComparatorImpt implements Comparator<Task> {
 		int result = 0;
 
 		if(task1.getImportance() == 3 && task2.getImportance() == null) {
-			return 0;
+			result = 0;
 		}
 
 		else if(task1.getImportance() == null && task2.getImportance() == 3) {
-			return 0;
+			result = 0;
 		}
 
 		else if(task1.getImportance() == null && task2.getImportance() == null) {
-			return 0;
+			result = 0;
 		}
 		else {
 			result = task1.getImportance().compareTo(task2.getImportance());
+		}
 
-			if(result == 0) {
-				result = task1.getTitle().compareTo(task2.getTitle());
-			}
+		if(result == 0) {
+			result = task1.getTitle().compareTo(task2.getTitle());
 		}
 		return result;
 	}
