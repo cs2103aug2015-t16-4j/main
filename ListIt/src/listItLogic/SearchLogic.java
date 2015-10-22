2f2f8c3fc8f8c653188a6755029087c8f6a34a60
zhangcs0088 <zhangcs00992008@gmail.com>
2015-10-22 13:45:49 +0800

    search by word implement

--- ListIt/src/listItLogic/SearchLogic.java
+++ ListIt/src/listItLogic/SearchLogic.java
@@ -1,12 +1,26 @@
 package listItLogic;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import fileModifier.FileModifier;
+import taskGenerator.Task;
+import taskGenerator.TaskComparatorDefault;
 
 public class SearchLogic {
 	
-	public static String searchKeyWord(String command) {
-		String keyword = command.substring(7); 
-		return keyword;
+	public static void searchKeyWord(String command) {
+		FileModifier modifier = FileModifier.getInstance();
+		
+		String keyword = command.substring(15);
+		
+		ArrayList<Task> taskList = modifier.searchKeyword(keyword);
+		
+		Collections.sort(taskList, new TaskComparatorDefault());
+		
+		modifier.display(taskList);
 	}
 	
 }
