a42a0b8c911af4b3b20219483f2deeaa8c59d18e
zhangcs0088 <zhangcs00992008@gmail.com>
2015-10-25 21:46:59 +0800

    able to move the window with mouse

--- ListIt/src/listItUI/UIMain.java
+++ ListIt/src/listItUI/UIMain.java
@@ -2,16 +2,21 @@ package listItUI;
 
 import fileModifier.FileModifier;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class UIMain extends Application{
+public class UIMain extends Application {
 	
 	InputTextPane inputBox;
 	OutputScreenPane screenBox;
 	FeedbackPane FeedbackBox;
 	TopBar topBar;
+	private double xOffset = 0;
+    private double yOffset = 0;
 	static FileModifier modifier = FileModifier.getInstance();
 
 	public static void main(String[] args) {
@@ -22,6 +27,7 @@ public class UIMain extends Application{
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.setTitle("ListIt");
+		primaryStage.initStyle(StageStyle.UNDECORATED);
 		
 		inputBox = new InputTextPane();
 		screenBox = new OutputScreenPane();
@@ -35,6 +41,22 @@ public class UIMain extends Application{
 		layout.setLeft(screenBox);
 		layout.setCenter(FeedbackBox);
 		layout.setBottom(inputBox);
+		
+		//Used to move undecorated window
+		layout.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset = event.getSceneX();
+                yOffset = event.getSceneY();
+            }
+        });
+        layout.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX() - xOffset);
+                primaryStage.setY(event.getScreenY() - yOffset);
+            }
+        });
 		
 		Scene scene = new Scene(layout, 630, 600);
 		scene.getStylesheets().add("listItUI/ListItUITheme.css");
