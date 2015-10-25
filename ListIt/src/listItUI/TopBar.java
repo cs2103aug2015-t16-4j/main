bd34d15bb9a5f68525816bb901f9b01c8e461205
zhangcs0088 <zhangcs00992008@gmail.com>
2015-10-25 21:01:59 +0800

    refine topBar

--- ListIt/src/listItUI/TopBar.java
+++ ListIt/src/listItUI/TopBar.java
@@ -1,13 +1,16 @@
 package listItUI;
 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.Text;
 
-public class TopBar extends GridPane{
+public class TopBar extends GridPane implements EventHandler<ActionEvent>{
 	
 	private Text listItLabel;
 	private Button closeButton;
@@ -21,12 +24,28 @@ public class TopBar extends GridPane{
 				+ "-fx-stroke: black;");
 		
 		closeButton = new Button("Close");
-		minimizeButton = new Button("Hide");
+		minimizeButton = new Button("Hide");	
+		closeButton.setOnAction(this);
 		
 		setConstraints(listItLabel, 0, 0);
 		setConstraints(closeButton, 1, 0);
 		setConstraints(minimizeButton, 2, 0);
 		
+		ColumnConstraints col1Constraints = new ColumnConstraints();
+		col1Constraints.setPercentWidth(80);
+		ColumnConstraints col2Constraints = new ColumnConstraints();
+		col2Constraints.setPercentWidth(10);
+		ColumnConstraints col3Constraints = new ColumnConstraints();
+		col3Constraints.setPercentWidth(10);
+		
+		getColumnConstraints().addAll(col1Constraints, col2Constraints, col3Constraints);
 		getChildren().addAll(listItLabel, closeButton, minimizeButton);
+	}
+
+	@Override
+	public void handle(ActionEvent event) {
+		if(event.getSource() == closeButton) {
+			System.exit(0);
+		}
 	}
 }
