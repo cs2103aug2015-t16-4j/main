1b31a881caf0638ca661b2a99d59c6647ddc4983
Shrestha Goswami <a0130280@u.nus.edu>
2015-10-26 12:19:24 +0800

    refractoring and java coding standard for edit logic class

--- ListIt/src/listItLogic/EditLogic.java
+++ ListIt/src/listItLogic/EditLogic.java
@@ -6,37 +6,73 @@ import listItUI.FeedbackPane;
 public class EditLogic {
 
 	private static FileModifier modifier = FileModifier.getInstance();
+	private static final String COMMAND_TITLE = "by title";
+	private static final String COMMAND_IMPORTANCE = "by impt";
+	private static final String COMMAND_DEADLINE = "by date";
+	private static final String COMMAND_TIMELINE = "by time";
+	private static final String COMMAND_TO = "to";
+	private static final String COMMAND_FROM = "from";
 
 	public static void editEvent(String command) {
-		int IndexToBeEdit = Integer.parseInt(command.substring(5, 6));
+		int IndexToBeEdit = convertStringIndexToInt(command);
 
-		if (command.contains("by date")) {
-			String newDate = command.substring(command.indexOf("by date") + 8);
-
+		if (isEditByDate(command)) {
+			String newDate = getNewDate(command);
 			if (AddLogic.isValidDate(newDate)) {
 				modifier.editDate(IndexToBeEdit-1, newDate);
-			}
-
-			else {
+			} else {
 				FeedbackPane.displayInvalidDate();
 			}
-		} else if (command.contains("by title")) {
-			String newTitle = command.substring(command.indexOf("by title") + 9);
-
+		} else if (isEditByTitle(command)) {
+			String newTitle = getNewTitle(command);
 			modifier.editTitle(IndexToBeEdit - 1, newTitle);
-			
-		} else if (command.contains("by importance")) {
-			String newImportance = command.substring(command.indexOf("importance") + 11);
-
+		} else if (isEditByImportance(command)) {
+			String newImportance = getNewImportanceLevel(command);
 			modifier.editImportance(IndexToBeEdit - 1, newImportance);
-			
-		} else if (command.contains("by time")) {
-			String newStartTime = command.substring(command.indexOf("from") + 5, command.indexOf("to") - 1);
-			String newEndTime = command.substring(command.indexOf("to") + 3);
-
+		} else if (isEditByTimeline(command)) {
+			String newStartTime = getNewStartTime(command);
+			String newEndTime = getNewEndTime(command);
 			modifier.editTime(IndexToBeEdit - 1, newStartTime, newEndTime);
-			
 		} 
-		
+	}
+
+	private static String getNewEndTime(String command) {
+		return command.substring(command.indexOf(COMMAND_TO) + 3);
+	}
+
+	private static String getNewStartTime(String command) {
+		return command.substring(command.indexOf(COMMAND_FROM) + 5, command.indexOf(COMMAND_TO) - 1);
+	}
+
+	private static String getNewImportanceLevel(String command) {
+		return command.substring(command.indexOf(COMMAND_IMPORTANCE) + 11);
+	}
+
+	private static String getNewTitle(String command) {
+		return command.substring(command.indexOf(COMMAND_TITLE) + 9);
+	}
+
+	private static String getNewDate(String command) {
+		return command.substring(command.indexOf(COMMAND_DEADLINE) + 8);
+	}
+
+	private static boolean isEditByTimeline(String command) {
+		return command.contains(COMMAND_TIMELINE);
+	}
+
+	private static boolean isEditByImportance(String command) {
+		return command.contains(COMMAND_IMPORTANCE);
+	}
+
+	private static boolean isEditByTitle(String command) {
+		return command.contains(COMMAND_TITLE);
+	}
+
+	private static boolean isEditByDate(String command) {
+		return command.contains(COMMAND_DEADLINE);
+	}
+
+	private static int convertStringIndexToInt(String command) {
+		return Integer.parseInt(command.substring(5, 6));
 	}
 }
