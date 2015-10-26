8f8e4b43393eaf74a67ba6bf6d42f6fdfc0cebe6
Shrestha Goswami <a0130280@u.nus.edu>
2015-10-26 13:14:40 +0800

    refractoring and modified to ensure java coding standard for search logic class

--- ListIt/src/listItLogic/SearchLogic.java
+++ ListIt/src/listItLogic/SearchLogic.java
@@ -1,15 +1,14 @@
 package listItLogic;
 
 import java.util.ArrayList;
-import java.util.Collections;
-
 import fileModifier.FileModifier;
 import taskGenerator.Task;
-import taskGenerator.TaskComparatorAlpha;
-import taskGenerator.TaskComparatorDefault;
-import taskGenerator.TaskComparatorImpt;
 
 public class SearchLogic {
+	private static final String SEARCH_DATE = "date";
+	private static final String SEARCH_DEFAULT = "default";
+	private static final String SEARCH_IMPT = "impt";
+	private static final String SEARCH_ALPHA = "alpha";
 	
 	public static void searchKeyWord(String command) {
 		FileModifier modifier = FileModifier.getInstance();
@@ -17,34 +16,38 @@ public class SearchLogic {
 		
 		String keyword = command.substring(7);
 		
-		if (keyword.indexOf("date") == 0) {
-			keyword = keyword.substring(5);
+		if (keyword.indexOf(SEARCH_DATE) == 0) {
+			keyword = getKeyword(keyword);
 			taskList = modifier.searchByDate(keyword);
 			
-			modifier.setViewMode("default");
-			modifier.sort(taskList);
+			modifier.setViewMode(SEARCH_DEFAULT);
+			sortAndDisplaySearchList(modifier, taskList);
 			
-			modifier.display(taskList);
-			
-		} 
-		else if (keyword.indexOf("impt") == 0) {
-			keyword = keyword.substring(5);
-			int imptLevel = Integer.parseInt(keyword);
+		} else if (keyword.indexOf(SEARCH_IMPT) == 0) {
+			keyword = getKeyword(keyword);
+			int imptLevel = convertStringImptLevelToInt(keyword);
 			taskList = modifier.searchByImportance(imptLevel);
 			
-			modifier.setViewMode("impt");
-			modifier.sort(taskList);
-			
-			modifier.display(taskList);
-		} 
-		else {
+			modifier.setViewMode(SEARCH_IMPT);
+			sortAndDisplaySearchList(modifier, taskList);
+		} else {
 			taskList = modifier.searchKeyword(keyword);
 			
-			modifier.setViewMode("alpha");
-			modifier.sort(taskList);
-
-			modifier.display(taskList);
+			modifier.setViewMode(SEARCH_ALPHA);
+			sortAndDisplaySearchList(modifier, taskList);
 		}
 	}
-	
+
+	private static void sortAndDisplaySearchList(FileModifier modifier, ArrayList<Task> taskList) {
+		modifier.sort(taskList);
+		modifier.display(taskList);
+	}
+
+	private static String getKeyword(String keyword) {
+		return keyword.substring(5);
+	}
+
+	private static int convertStringImptLevelToInt(String keyword) {
+		return Integer.parseInt(keyword);
+	}
 }
