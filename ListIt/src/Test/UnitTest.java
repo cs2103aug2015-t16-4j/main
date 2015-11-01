80c313d498ae3b1188c59fd4ddcdd8ce16488d3a
urvashisikka <aditiverma@Aditis-MacBook-Pro.local>
2015-11-01 12:49:24 +0800

    add , delete and sort testing

--- ListIt/src/Test/UnitTest.java
+++ ListIt/src/Test/UnitTest.java
@@ -14,7 +14,7 @@ import listItLogic.SearchLogic;
 import taskGenerator.Task;
 
 public class UnitTest {
-	
+
 	FileModifier modifier = FileModifier.getInstance();
 
 	@Test
@@ -26,136 +26,51 @@ public class UnitTest {
 		String deleteMessage = null;
 		String addDeadlineMessage = null;
 		String addRankMessage = null;
-		
-		DeleteLogic.clearFile();
-		testDeleteLogicClear(expected, "test clear");
-		
-		Task task1 = new Task("EE2020 Oscilloscope project", "03112015");
-		expected.add(task1);
-		AddLogic.addEventWithDeadline("add Complete EE2020 oscilloscope project by 03112015");
-		testAddWithDeadlineLogic(modifier.getContentList(), expected, 
-				                 "test add with deadline logic");
-		
-		Task task2 = new Task("OP2 presentation", "06112015");
-		expected.add(task2);
-		AddLogic.addEventWithDeadline("add Oral Presentation 2 of Software Demo by 06112015");
-		
-		DeleteLogic.deleteEvent("delete 3");
-		deleteMessage = DeleteLogic.getMessage();
-	    testDeleteLogicDeleteEvent("Index is out of bounds", deleteMessage, expected, 
-				                   "test delete 3");
-		DeleteLogic.deleteEvent("delete 0");
-		deleteMessage = DeleteLogic.getMessage();
-		testDeleteLogicDeleteEvent("Index is out of bounds", deleteMessage, expected, 
-				                   "test delete 0");
-		
-		DeleteLogic.deleteEvent("delete 2");
-		deleteMessage = DeleteLogic.getMessage();
-		testDeleteLogicDeleteEvent("null", deleteMessage, expected, "test delete 2");
-		
-		
-		AddLogic.addEventDefault("add "); 
-		addMessage = AddLogic.getMessage();
-		testAddDefaultLogic("test default add", expected, "Please enter an event title", addMessage);
-		
-		AddLogic.addEventWithDeadline("add complete EE2020 lab report by next Friday");
-		addDeadlineMessage = AddLogic.getDeadlineMessage();
-		testAddDeadlineLogic("test adding with deadline in wrong format", expected, 
-				             addDeadlineMessage, "Please enter a valid date! Days of the week are not accepted");
-		
-		AddLogic.addEventWithDeadline("add go for light and sound show at the Gardens by the Bay");
-		Task task3 = new Task("go for light and sound show a the Gardens by the Bay");
-		expected.add(task3);
-		addDeadlineMessage = AddLogic.getDeadlineMessage();
-		testAddDeadlineLogic("testing adding floating task with the word by", expected, 
-				             addDeadlineMessage, null);
-		
-		AddLogic.addEventWithDeadline("add attend meeting by 152015");
-		addDeadlineMessage = AddLogic.getDeadlineMessage();
-		testAddDeadlineLogic("testing adding with deadline with wrong date format", expected,
-				             addDeadlineMessage, "enter a valid date");
-		
-		AddLogic.addEventWithImportance("add watch movie rank 0");
-		addRankMessage = AddLogic.getRankMessage();
-		testAddRankLogic("test adding with wrong rank range", expected, addRankMessage, "invalid rank input");
-		
-		AddLogic.addEventWithImportance("add go for manicure rank 4");
-		addRankMessage = AddLogic.getRankMessage();
-		testAddRankLogic("test adding with wrong rank range", expected, addRankMessage,
-				         "invalid rank input");
-		
-		AddLogic.addEventWithImportance("add do groceries rank 3");
-		addRankMessage = AddLogic.getRankMessage();
-		Task task4 = new Task("do groceries", 3);
-		expected.add(task4);
-		testAddRankLogic("test adding with right rank range", expected, addRankMessage,
-				         "invalid rank input");
-		
-		SearchLogic.searchKeyWord("search date 03112015");
-		actualSearchList = SearchLogic.getTaskList();
-		testSearchLogicDate("test search by date" , expectedSearchList, actualSearchList); 
-		
-		//testing a date not present 
-		SearchLogic.searchKeyWord("search data 05112015"); 
-		testSearchLogicDate("test search date which is not present" , expected); 
-		
-	   //test for importance ( 1 & 4) 
-		SearchLogic.searchKeyWord("search impt 3");
-		
-		SearchLogic.searchKeyWord("search impt 4");
+		String searchMessage = null; 
 
-	   //test key word present and not present . 
-		SearchLogic.searchKeyWord("search Oral Presentation 2"); 
-		
-		SearchLogic.searchKeyWord("search tutorial work"); 
+		DeleteLogic.clearFile();
+		testDeleteLogicClear(expected, "test clear"); 
+
+		Task task1 = new Task("EE2020 Oscilloscope project", "03112015","1");
+		Task task2 = new Task("OP2 presentation", "06112015","3");
+		expected.add(task1); 
+		expected.add(task2); 
+
+		// this tests the different types of add 
+		testAddDefault(addMessage , expected); 
+		testAddWithDeadline(addDeadlineMessage , expected); 
+		testAddWithImpt(expected , addRankMessage); 
+
+		testDelete(deleteMessage ,expected); 
+		//testSearch(searchMessage ,expectedSearchList , actualSearchList); 
 		testSort(expected,modifier.getContentList() ); 
-	}
-	
-	private void testAddRankLogic(String description, ArrayList<Task> expected, String actualMessage,
-			                      String expectedMessage) {
-		if(actualMessage != null) {
-			assertEquals(description, expectedMessage, actualMessage);
-		} else {
-			ArrayList<Task> actual = modifier.getContentList();
-			assertEquals(description, expected, actual);
-		}
-	}
-	
-	private void testAddDeadlineLogic(String description, ArrayList<Task> expected, String actualMessage,
-			                          String expectedMessage) {
-		if(actualMessage != null) {
-			assertEquals(description, expectedMessage, actualMessage);
-		} else {
-			ArrayList<Task> actual = modifier.getContentList();
-			assertEquals(description, expected, actual);
-		}
 	}
 
 	private void testSort(ArrayList<Task> expected ,ArrayList<Task> actual ) {
 		String description = "test if sort works"; 		
 		assertEquals(description , expected , actual); 		
 	}
+	private void testDelete(String deleteMessage, ArrayList<Task> expected) {
 
-	private void testSearchLogicDate(String description, ArrayList<Task> expectedSearch, ArrayList<Task> actualSearch) {
-		 assertEquals(description, expectedSearch, actualSearch);
+		AddLogic.addEventWithDeadline("add Complete EE2020 oscilloscope project by 03112015 rank 1");
+		AddLogic.addEventWithDeadline("OP2 presentation by 06112015 rank 3");
+
+		DeleteLogic.deleteEvent("delete 3");
+		deleteMessage = DeleteLogic.getMessage();
+		testDeleteLogicDeleteEvent("Index is out of bounds", deleteMessage, expected, 
+				"test delete 3");
+		DeleteLogic.deleteEvent("delete 0");
+		deleteMessage = DeleteLogic.getMessage();
+		testDeleteLogicDeleteEvent("Index is out of bounds", deleteMessage, expected, 
+				"test delete 0");
+
+		DeleteLogic.deleteEvent("delete 2");
+		deleteMessage = DeleteLogic.getMessage();
+		testDeleteLogicDeleteEvent("null", deleteMessage, expected, "test delete 2");	
 	}
 
-	private void testAddDefaultLogic(String description, ArrayList<Task> expected, String expectedMessage, String actualMessage) {
-		if(actualMessage == null) {
-			ArrayList<Task> actual = modifier.getContentList();
-			assertEquals(description, expected, actual);
-		} else {
-			assertEquals(description, expectedMessage, actualMessage);
-		}
-	}
-    
-	private void testDeleteLogicClear(ArrayList<Task> expected, String description) {
-		ArrayList<Task> actual = modifier.getContentList();
-		assertEquals(description, expected.size(), actual.size());
-	}
-	
 	private void testDeleteLogicDeleteEvent(String expectedMessage, String actualMessage,  
-			                                ArrayList<Task> expected, String description) {
+			ArrayList<Task> expected, String description) {
 		if(actualMessage != null) {
 			assertEquals(description, expectedMessage, actualMessage);
 		} else {
@@ -165,9 +80,65 @@ public class UnitTest {
 			assertEquals(description, expected, actual);
 		}
 	}
-	
-	private void testAddWithDeadlineLogic(ArrayList<Task> actual, ArrayList<Task> expected, 
-			                              String description) {
-	    assertEquals(description, expected, actual);	
+
+
+	private void testAddWithImpt(ArrayList<Task> expected , String addRankMessage){
+
+		//this test the invalid inputs (boundary cases) 
+		AddLogic.addEventWithImportance("add watch movie rank 0");
+		addRankMessage = AddLogic.getRankMessage();
+		testAddRankLogic("test adding with wrong rank range", expected, addRankMessage, "invalid rank input");
+
+		AddLogic.addEventWithImportance("add go for manicure rank 4");
+		addRankMessage = AddLogic.getRankMessage();
+		testAddRankLogic("test adding with wrong rank range", expected, addRankMessage,
+				"invalid rank input");
+
+		AddLogic.addEventWithImportance("OP2 presentation by 06112015 rank 3");
+		addRankMessage = AddLogic.getRankMessage();
+		testAddRankLogic("test adding with right rank range", expected, addRankMessage,
+				"invalid rank input");		
 	}
-}
+
+	private void testAddRankLogic(String description, ArrayList<Task> expected, String actualMessage,
+			String expectedMessage) {
+		if(actualMessage != null) {
+			assertEquals(description, expectedMessage, actualMessage);
+		} else {
+			ArrayList<Task> actual = modifier.getContentList();
+			assertEquals(description, expected, actual);
+		}
+	}
+
+	private void testAddWithDeadline(String addMessage , ArrayList<Task> expected){
+		ArrayList<Task> actual = new ArrayList<Task>(); 
+		AddLogic.addEventWithDeadline("add Complete EE2020 oscilloscope project by 03112015 rank 1");
+		AddLogic.addEventWithDeadline("OP2 presentation by 06112015 rank 3");
+		actual = modifier.getContentList(); 
+		testAddWithDeadlineLogic(actual, expected, "test add with deadline logic");
+	}
+
+	private void testAddWithDeadlineLogic(ArrayList<Task> actual, ArrayList<Task> expected, 
+			String description) {
+		assertEquals(description, expected, actual);	
+	}
+
+
+	private void testAddDefault(String addMessage , ArrayList<Task> expected){
+		AddLogic.addEventDefault("add "); 
+		addMessage = AddLogic.getMessage();
+		testAddDefaultLogic("test default add", expected, "Please enter an event title", addMessage);	
+	}
+	private void testAddDefaultLogic(String description, ArrayList<Task> expected, String expectedMessage, String actualMessage) {
+		if(actualMessage == null) {
+			ArrayList<Task> actual = modifier.getContentList();
+			assertEquals(description, expected, actual);
+		} else {
+			assertEquals(description, expectedMessage, actualMessage);
+		}
+	}
+	private void testDeleteLogicClear(ArrayList<Task> expected, String description) {
+		ArrayList<Task> actual = modifier.getContentList();
+		assertEquals(description, expected.size(), actual.size());
+	}
+} 
