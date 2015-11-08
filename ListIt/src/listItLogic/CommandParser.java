f9600c677c96450ee8a4163f15f4a4d5edb79074
shawnyeun <a0124181@nus.edu.sg>
2015-11-08 19:26:37 +0800

    Comments for CommandParser and Complete

--- ListIt/src/listItLogic/CommandParser.java
+++ ListIt/src/listItLogic/CommandParser.java
@@ -1,5 +1,12 @@
 package listItLogic;
 
+/**
+ * This class contains methods which parses the user commands by checking if the 
+ * command has white space (more than 1 word), or has no white space (1 word only). 
+ * Then, it can execute the command. 
+ * @author Shrestha
+ * @version 0.5
+ */
 public class CommandParser {
 	private static final String WHITESPACE = " ";
 	
@@ -7,6 +14,14 @@ public class CommandParser {
 		
 	}
 	
+	/**
+	 * This method parses the user commands by checking if the  
+     * command has white space (more than 1 word), or has no white space (1 word only).
+	 * @param command string command input by the user
+	 * @throws InvalidCommandException
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 */
 	public static void processCommand(String command) {
 		
 		assert command != null;
@@ -18,6 +33,11 @@ public class CommandParser {
 		}
 	}
 
+	/**
+	 * Checks if the command has a white space or not.
+	 * @param command string command input by the user
+	 * @return true if the command string has a white space, else returns false.
+	 */
 	private static boolean hasWhitespace(String command) {
 		return command.contains(WHITESPACE);
 	}
--- ListIt/src/listItLogic/CompleteLogic.java
+++ ListIt/src/listItLogic/CompleteLogic.java
@@ -3,6 +3,12 @@ package listItLogic;
 import fileModifier.FileModifier;
 import listItUI.FeedbackPane;
 
+/**
+ * This class contains methods to move tasks from the usual data file, 
+ * into the completed data file, when the user enters "complete". 
+ * @author Shrestha
+ *
+ */
 public class CompleteLogic {
 	
 	private static FileModifier modifier = FileModifier.getInstance();
@@ -10,6 +16,11 @@ public class CompleteLogic {
 	private static final String MESSAGE_OUT_OF_BOUNDS = "Index is out of bounds";
 	private static final String COMPLETE_SUCCESSFUL = "Task is completed";
 
+	/**
+	 * This method gets the task by using the line index, and then moves the task
+	 * over to the completed task list. 
+	 * @param command string command input by the user with a "complete" keyword.
+	 */
 	public static void completeEvent(String command) {
 		int taskIndexComplete = getCompleteIndex(command);
 		int sizeOfFile = getSizeOfFile();
@@ -25,6 +36,12 @@ public class CompleteLogic {
 		}
 	}
 
+	/**
+	 * Checks if the index entered is valid (in the range of the task list)
+	 * @param taskIndexComplete completed task list index
+	 * @param sizeOfFile size of the task list to get the index range
+	 * @return
+	 */
 	private static boolean isValidIndex(int taskIndexComplete, int sizeOfFile) {
 		return (taskIndexComplete-1) < sizeOfFile && taskIndexComplete-1 >= 0;
 	}
