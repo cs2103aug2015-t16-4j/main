6c005f9f5fa0e3c2ce13fe5810def1784e380147
shawnyeun <a0124181@nus.edu.sg>
2015-11-08 19:55:44 +0800

    Comments for SeachLogic

--- ListIt/src/listItLogic/SearchLogic.java
+++ ListIt/src/listItLogic/SearchLogic.java
@@ -5,6 +5,13 @@ import fileModifier.FileModifier;
 import listItUI.FeedbackPane;
 import taskGenerator.Task;
 
+/**
+ * This class contains methods to search for the user input keyword, to find that keyword
+ * in our task list. Keyword can be in the form of importance variable, date variable or 
+ * event title. 
+ * @author Shrestha
+ * @version 0.5
+ */
 public class SearchLogic {
 	private static final String SEARCH_DATE = "date";
 	private static final String SEARCH_DEFAULT = "default";
@@ -22,6 +29,12 @@ public class SearchLogic {
 	private static ArrayList<Task> taskList = new ArrayList<Task>();
 	private static String message = "null";
 
+	/**
+	 * This method determines the type of search keyword the user has input, and
+	 * checks if the user has input it in the correct format. After which, it sorts
+	 * and displays the searched task list to the user. 
+	 * @param command String command input by the user with a "search" keyword.
+	 */
 	public static void searchKeyWord(String command) {
 		FileModifier modifier = FileModifier.getInstance();
 
@@ -70,26 +83,56 @@ public class SearchLogic {
 		}
 	}
 
+	/**
+	 * Checks if the importance level is rank 3
+	 * @param imptLevel importance level
+	 * @return true if importance level is rank 3
+	 */
 	private static boolean isNotImportant(int imptLevel) {
 		return imptLevel == IMPORTANCE_LEVEL_THREE;
 	}
 
+	/**
+	 * Checks if the importance level is rank 2
+	 * @param imptLevel importance level
+	 * @return true if importance level is rank 2
+	 */
 	private static boolean isImportant(int imptLevel) {
 		return imptLevel == IMPORTANCE_LEVEL_TWO;
 	}
 
+	/**
+	 * Checks if the importance level is rank 1
+	 * @param imptLevel importance level
+	 * @return true if importance level is rank 1
+	 */
 	private static boolean isVeryImportant(int imptLevel) {
 		return imptLevel == IMPORTANCE_LEVEL_ONE;
 	}
 
+	/**
+	 * Checks if the keyword in the command is "impt"
+	 * @param keyword
+	 * @return true if the keyword is "impt"
+	 */
 	private static boolean isSearchByImportance(String keyword) {
 		return keyword.indexOf(SEARCH_IMPT) == 0;
 	}
 
+	/**
+	 * Checks if the keyword in the command is "date"
+	 * @param keyword
+	 * @return true if the keyword is "date"
+	 */
 	private static boolean isSearchByDate(String keyword) {
 		return keyword.indexOf(SEARCH_DATE) == 0;
 	}
 
+	/**
+	 * Checks if the search task list is an empty task list or not.
+	 * @param taskList2 the task list that has the search keyword.
+	 * @return true if task list is empty, else returns false.
+	 */
 	private static boolean isTaskListEmpty(ArrayList<Task> taskList2) {
 		if (taskList.isEmpty()) {
 			return true;
@@ -97,6 +140,11 @@ public class SearchLogic {
 		return false;
 	}
 
+	/**
+	 * Sorts and displays the search task list.
+	 * @param modifier the modifier variable in our FileModifier class
+	 * @param taskList task list that has the search keyword
+	 */
 	private static void sortAndDisplaySearchList(FileModifier modifier, ArrayList<Task> taskList) {
 		modifier.sort(taskList);
 		modifier.display(taskList);
