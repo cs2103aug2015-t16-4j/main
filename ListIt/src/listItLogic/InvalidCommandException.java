0fdd0f604980e60814a783da293f6f34a5bf1a50
shawnyeun <a0124181@nus.edu.sg>
2015-11-08 19:50:31 +0800

    Comments for InvalidCommandException

--- ListIt/src/listItLogic/InvalidCommandException.java
+++ ListIt/src/listItLogic/InvalidCommandException.java
@@ -1,5 +1,12 @@
 package listItLogic;
 
+/**
+ * This class is used to extend the exceptions that occurs in ListIt
+ * by creating an InvalidCommandException, therefore, providing a 
+ * code of higher quality.
+ * @author Shrestha
+ *
+ */
 @SuppressWarnings("serial")
 public class InvalidCommandException extends Exception{
 	
