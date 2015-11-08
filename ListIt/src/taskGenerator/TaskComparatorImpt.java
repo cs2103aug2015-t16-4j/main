9dfc4e4325ee45d937e2ec0d51268b83ebaf10d7
shawnyeun <a0124181@nus.edu.sg>
2015-11-08 20:04:41 +0800

    Comments for TaskComparatorImpt

--- ListIt/src/taskGenerator/TaskComparatorImpt.java
+++ ListIt/src/taskGenerator/TaskComparatorImpt.java
@@ -2,8 +2,23 @@ package taskGenerator;
 
 import java.util.Comparator;
 
+/**
+This class updates the collection.sort method in order to compare and
+* sort tasks by the importance variable. The higher the importance value,
+* the task is sorted higher up the list
+* @author Shawn
+* @version 0.5
+*/
 public class TaskComparatorImpt implements Comparator<Task> {
 
+	/**
+	 * This method compares 2 tasks by importance. if the importance are the same,
+	 * or if only 1 or 0 tasks have an importance variable, the result returned = 0. 
+	 * another sorting method to compare by the title is called. 
+	 * @param task1 The first task to be compared
+	 * @param task2 the second task to be compared
+	 * @return result the value of 0 (equals to), -1 (less than) or 1 (greater than) 
+	 */
 	@Override
 	public int compare(Task task1, Task task2) {
 		int result = 0;
@@ -24,6 +39,12 @@ public class TaskComparatorImpt implements Comparator<Task> {
 		return result;
 	}
 
+	/**
+	 * This method checks if the compared result of the importance value
+	 * =0, meaning they have the same importance level.
+	 * @param result compared importance result
+	 * @return result to be 0 (equals to)
+	 */
 	private boolean isResultZero(int result) {
 		return result == 0;
 	}
