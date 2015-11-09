eaadb76a9c11b4f6ce3a99bae13d7ba2399f84de
urvashisikka <aditiverma@Aditis-MacBook-Pro.local>
2015-11-09 13:01:29 +0800

    logging comments

--- ListIt/src/listItLogic/LoggingLogic.java
+++ ListIt/src/listItLogic/LoggingLogic.java
@@ -1,5 +1,14 @@
 package listItLogic;
 
+/**
+ * This class just performs the simple task of creating 
+ * a log that prints out a message on the console. 
+ * This is done for analysis purposes. 
+ * 
+ * @author Shrestha
+ * @version 0.5
+ */
+
 public class LoggingLogic {
 	
 	public static void logging(String message){
